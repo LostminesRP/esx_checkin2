8633fb85fa696b8d2f35bc61c31f583f21538ca5
VictorNJ21 <65968706+VictorNJ21@users.noreply.github.com>
2020-08-09 22:53:17 +0200

    Update no.lua

--- locales/no.lua
+++ locales/no.lua
@@ -24,6 +24,7 @@ Locales['no'] = {
     ['policebadge_subject'] = 'Los Santos Stat',
     ['policebadge_job'] = '~b~Jobb~w~: ',
     ['policebadge_job_grade'] = '\n~b~Jobb Grad~w~: ',
+    ['gave_policebadge'] = 'Du viste politimerket ditt.',
      -- Ingen nærme
      ['no_one_nearby'] = 'Ingen spiller(e) nærme!',
 }
